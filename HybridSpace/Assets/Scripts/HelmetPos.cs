c15a412d3487a6560a0098e1cedbcf1d6a423b88
Wridzer Kamphuis <wridzer12@gmail.com>
2021-12-24 12:55:28 +0100

    helmet works

--- HybridSpace/Assets/Scripts/HelmetPos.cs
+++ HybridSpace/Assets/Scripts/HelmetPos.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class HelmetPos : MonoBehaviour
 {
     [SerializeField] private GameObject cameraInstance;
-    [SerializeField] private Vector3 offset = new Vector3(0, -0.045f, 0);
+    [SerializeField] private GameObject rightHand;
+    [SerializeField] private GameObject leftHand;
+    [SerializeField] private Vector3 offsetPos = new Vector3(0, -0.045f, 0);
+    [SerializeField] private Vector3 offsetRot = new Vector3(0, 90, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,11 @@ public class HelmetPos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Camera.main.transform.position + offset;
+        float rotateSpeed = 5 * Time.deltaTime;
+        Vector3 difference = rightHand.transform.position - leftHand.transform.position;
+        transform.position = Camera.main.transform.position + offsetPos;
+        Vector3 puntD = new Vector3(leftHand.transform.position.x + (difference.x * 0.5f), transform.position.y, leftHand.transform.position.z + (difference.z * 0.5f));
+        Vector3 lookDir = Vector3.RotateTowards(transform.forward, transform.position - puntD, rotateSpeed, 0f);
+        transform.rotation = Quaternion.LookRotation(lookDir);
     }
 }
