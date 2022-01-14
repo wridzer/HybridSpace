ca36d643151e2bbc425392e7bf67bf03385c4055
Wridzer Kamphuis <wridzer12@gmail.com>
2022-01-14 16:50:26 +0100

    networking now works with VR B-)

---
+++ HybridSpace/Assets/Scripts/Photon/FollowDiver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FollowDiver : MonoBehaviour
+{
+    public GameObject diver;
+    [SerializeField] private Vector3 offset = new Vector3(0.5f, 1.5f, 0f);
+
+    private void Update()
+    {
+        if(diver != null)
+        {
+            transform.position = diver.transform.position + offset;
+        } else
+        {
+            diver = RoomManager.diver;
+        }
+    }
+}
--- HybridSpace/Assets/Scripts/Photon/PlayerManager.cs
+++ HybridSpace/Assets/Scripts/Photon/PlayerManager.cs
@@ -8,7 +8,6 @@ using System.Linq;
 public class PlayerManager : MonoBehaviour
 {
     private PhotonView PV;
-    private GameObject host;
 
     private void Awake()
     {
@@ -27,13 +26,11 @@ public class PlayerManager : MonoBehaviour
     {
         if (PhotonNetwork.IsMasterClient)
         {
-            host = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Diver"), new Vector3(50, 2, 50), Quaternion.identity);
-            RoomManager.SetHost(host);
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Diver"), new Vector3(50, 2, 50), Quaternion.identity);
         } else
         {
             GameObject player = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "TestPlayer"), new Vector3(50, 2, 50), Quaternion.identity);
-            player.transform.parent = this.transform.Find("Diver").GetComponent<Transform>();
-            player.transform.localPosition = new Vector3(0.5f, 1.5f, 0f);
+
         }
     }
 }
