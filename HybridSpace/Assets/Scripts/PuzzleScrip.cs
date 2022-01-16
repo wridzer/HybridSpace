5bd6cbbf361332f1c927f4dadee256eef866a73e
Sean-G <62131291+Mr-Singa@users.noreply.github.com>
2022-01-16 15:37:19 +0100

    valve minigame upgraded

--- HybridSpace/Assets/Scripts/PuzzleScrip.cs
+++ HybridSpace/Assets/Scripts/PuzzleScrip.cs
@@ -5,32 +5,59 @@ using UnityEngine.UI;
 
 public class PuzzleScrip : MonoBehaviour
 {
-    public float sliderAmount;
+    public float sliderDecrease;
+    public float sliderIncrease;
     public float decreaseTime;
     public float cooldownTime;
+    public float redZone;
 
     public bool cooldown;
 
     public KeyCode keyCode;
 
     public Slider slider;
+    public Color sliderColor;
+
+    public float colorTimer;
+    public Image fill;
+
+    public bool higher;
 
     void Start()
     {
         slider = GetComponent<Slider>();
+        //fill = GetComponent<Slider>();
 
         StartCoroutine(ReduceSlider());
+
+        sliderColor = fill.color;
     }
 
     void Update()
     {
-        //if correct key is pressed increase slider amount
-        if (Input.GetKey(keyCode) && cooldown == false)
+        //if correct key is held down increase slider amount
+        if (Input.GetKey(keyCode))
         {
-            slider.value += sliderAmount;
-            cooldown = true;
-            Invoke("ResetCooldown", cooldownTime);
+            slider.value += sliderIncrease;
         }
+
+        if(higher == false)
+        {
+            if (slider.value < redZone)
+            {
+                fill.color = Color.Lerp(sliderColor, Color.red, Mathf.PingPong(Time.time, colorTimer));
+            }
+            else { fill.color = sliderColor; }
+        }
+        else
+        {
+            if (slider.value > redZone)
+            {
+                fill.color = Color.Lerp(sliderColor, Color.red, Mathf.PingPong(Time.time, colorTimer));
+            }
+            else { fill.color = sliderColor; }
+        }
+        
     }
 
     IEnumerator ReduceSlider()
@@ -40,13 +67,7 @@ public class PuzzleScrip : MonoBehaviour
         {
             yield return new WaitForSeconds(decreaseTime);
 
-            slider.value -= sliderAmount;
+            slider.value -= sliderDecrease;
         }
-    }
-
-    public void ResetCooldown()
-    {
-        //resets cool down bool
-        cooldown = false;
     }
 }
