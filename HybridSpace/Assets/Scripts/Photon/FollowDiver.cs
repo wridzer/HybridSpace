71f0a6bdcae4c1fe1d72c24d791c49dc2223b718
Wridzer Kamphuis <wridzer12@gmail.com>
2022-01-17 13:12:47 +0100

    couldn't fix boatcam still

--- HybridSpace/Assets/Scripts/Photon/FollowDiver.cs
+++ HybridSpace/Assets/Scripts/Photon/FollowDiver.cs
@@ -3,17 +3,29 @@ using UnityEngine;
 
 public class FollowDiver : MonoBehaviour
 {
-    public GameObject diver;
+    public GameObject diver, helmet;
     [SerializeField] private Vector3 offset = new Vector3(0.5f, 1.5f, 0f);
 
     private void Update()
     {
         if(diver != null)
         {
-            GameObject helmet = diver.GetComponentInChildren<HelmetPos>().gameObject;
-            transform.position = helmet.transform.localPosition + offset;
-            transform.rotation = helmet.transform.rotation;
-        } else
+            if(helmet == null)
+            {
+                foreach (Transform child in diver.transform)
+                {
+                    if(child.GetComponentInChildren<HelmetPos>().gameObject != null)
+                    {
+                        helmet = child.GetComponentInChildren<HelmetPos>().gameObject;
+                        transform.parent = helmet.transform;
+                        gameObject.transform.position = offset;
+                    }
+                    if(helmet != null) { break; }
+                }
+            }
+            gameObject.transform.localPosition = offset;
+        } 
+        else
         {
             diver = RoomManager.diver;
         }
